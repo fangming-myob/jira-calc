b9be30c0eb4cb145b71a5e142d7567f301f8db81
Qilin Lou <Qilin.Lou@myob.com>
2019-12-25 19:02:04 +1100

    Update 2-getJiraReport.sh
    Former-commit-id: 869bc0a1bca739394f9cf1dfd98c9551ec3eae99

--- release/2-getJiraReport.sh
+++ release/2-getJiraReport.sh
@@ -1,1 +1,3 @@
-curl -X GET localhost:8081/jira/getCardsFile -H "$(cat ./app/jql.txt)" -H "$(cat ./app/token.txt)" -H "$(cat ./app/stages.txt)" > 4-result.csv
+now="$(date +"%Y-%m-%d %H:%M:%S")"
+#echo $now
+curl -X GET localhost:8081/jira/getCardsFile -H "$(cat ./app/jql.txt)" -H "$(cat ./app/token.txt)" -H "$(cat ./app/stages.txt)" > $now.csv
