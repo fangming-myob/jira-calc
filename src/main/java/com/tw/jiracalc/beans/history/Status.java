1c6e92b85ed285158926937d23465a9ddd9810b1
PhonyLou <1154537887@qq.com>
2020-03-12 17:18:44 +0800

    Use lombok instead of code

--- src/main/java/com/tw/jiracalc/beans/history/Status.java
+++ src/main/java/com/tw/jiracalc/beans/history/Status.java
@@ -1,15 +1,12 @@
 package com.tw.jiracalc.beans.history;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Status {
     private String displayValue;
-
-    public Status() {
-    }
-
-    public Status(String displayValue) {
-        this.displayValue = displayValue;
-    }
 }
